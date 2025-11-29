b77312eaf863668df8229739947614271fd7262b
charlottebe <cjbertanzetti@gmail.com>
2025-11-28 17:11:39 -0700

    added split function

--- main.cpp
+++ main.cpp
@@ -3,10 +3,28 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
 #include "Board.h"
 #include "Player.h"
 
 using namespace std;
+
+vector<string> split(string line, char delimiter){
+    vector<string> parts;
+    string temp="";
+    for(int i=0;i<line.length();i++){
+        if(line[i]==delimiter){
+            parts.push_back(temp);
+            temp="";
+        }
+        else{
+            temp+=line[i];
+
+        }
+    }
+    parts.push_back(temp);
+    return parts;
+}
 
 int main(){
 
